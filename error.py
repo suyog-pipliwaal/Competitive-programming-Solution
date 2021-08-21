518db1c096641c5b56e9e3d335ec1c0e7684b7d9
suyog-pipliwaal <suyogpipliwal@gmail.com>
2021-08-21 14:31:01 +0530

    delete files

--- error.py
+++
@@ -1,8 +0,0 @@
-if __name__ == '__main__':
-  testcase = int(raw_input())
-  for i in range(1,testcase+1,1):
-     string  = raw_input()
-     if "010" in string or "101" in string:
-        print ("Good")
-     else:
-        print ("Bad")
--- evengame.py
+++
@@ -1,8 +0,0 @@
-testcase = int(input())
-for i in range(testcase):
-    n = int(input())
-    arr = list(map(int, input().strip().split()))
-    if (sum(arr)%2==0):
-        print("1")
-    else:
-        print("2")
